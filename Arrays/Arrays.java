5c3e456146b36c83dc75a3f1327ee7bb39794e86
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-08 19:23:36 +0530

    Create Arrays.java

---
+++ Arrays/Arrays.java
@@ -0,0 +1,13 @@
+  
+public class Main {
+ public static void main(String[] args) {
+  
+   // create an array
+   int[] array = {12, 4, 5, 2, 5};
+
+   // print the array
+ for(int i=0;i<array.lenth();i++){
+ System.out.println(array[0]+",");
+ }
+ }
+}
